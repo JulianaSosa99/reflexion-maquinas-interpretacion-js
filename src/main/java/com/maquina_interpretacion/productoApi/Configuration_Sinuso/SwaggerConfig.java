0a39cd71fb2927ecd56c5be5a907584f26c9b34b
Juliana Sosa <juliana.sosa@udla.edu.ec>
2024-11-10 14:05:50 -0500

    comentarios de la clase Swagger Config

--- src/main/java/com/maquina_interpretacion/productoApi/Configuration_Sinuso/SwaggerConfig.java
+++ src/main/java/com/maquina_interpretacion/productoApi/Configuration_Sinuso/SwaggerConfig.java
@@ -1,5 +1,5 @@
 /*package com.maquina_interpretacion.productoApi.Configuration;
-
+//comento ya que no servia y decidi dejarlo para mejorarloo luego
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
